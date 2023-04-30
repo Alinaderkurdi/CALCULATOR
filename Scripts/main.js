aee646754dce1bdea1a6250ae3107172e4fc353b
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-04-30 15:19:39 +0430

    set up history section

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,14 @@
 const sideButton = document.querySelector('.side-bare-button')
 const sideSection = document.querySelector('.side-section')
 const historyButton = document.querySelector('.history-button')
+const historySection = document.querySelector('.history-section')
+
+
+
+historyButton.addEventListener('click', ()=>{
+   historySection.classList.add('history-section-open')
+})
+
 
 
 sideButton.addEventListener('click', (event)=>{
