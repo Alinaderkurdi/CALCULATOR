51fd0dabc7f3e538af29a90462ddcdab58a903dd
AliNaderkurdi <115383786+Alinaderkurdi@users.noreply.github.com>
2023-05-09 15:36:57 +0430

    add final

--- Scripts/type.ts
+++
@@ -1,9 +0,0 @@
-const me:string = 'Ali naderkurdi';
-const age : number = 22;
-
-const sayHello = (me:string, age: number)=>{
-  console.log(`HELLO ${me} your age is ${age} wolcome to our APP!!!`)
-}
-
-
-sayHello(me, age)
